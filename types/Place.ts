cbcf7c487dbf076779397d1ac0775fed0087fed1
Luan Silva <luan_barbosa_da_silva@outlook.com>
2023-05-15 21:31:25 -0300

    Update type image

--- types/Place.ts
+++ types/Place.ts
@@ -5,8 +5,8 @@ export type Place = {
     text2?: string; 
     text3?: string; 
     image1: string; 
-    image2?: string; 
-    image3?: string; 
+    image2: string; 
+    image3: string; 
     contact: string;
     address: string;
     maps?: string;
